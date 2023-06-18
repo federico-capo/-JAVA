6be529689a7f5c4daf7f47881b33eea1793b63f4
Federico <126673308+federico-capo@users.noreply.github.com>
2023-06-18 17:34:24 +0200

    update

--- Riassunto/src/Variabili/variabili.java
+++ Riassunto/src/Variabili/variabili.java
@@ -4,7 +4,21 @@ public class variabili {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int array[]= {10,20,30,40,50,100,300,90,22};
+		int counter= 0;
+		for(int i= 0; i<array.length;i++){
+			counter= counter+array[i];
+		}
+		System.out.println(counter);
 
+		//n3
+		int max = 0;
+		for(int i = 0; i<array.length;i++){
+			if(max<array[i]){
+				max= array[i];
+			}
+		}
+		System.out.println(max);
 	}
 
 }
